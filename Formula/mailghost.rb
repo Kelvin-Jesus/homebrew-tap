bdf6c6d0ae72b32b4e72d252e3a354e9e6fc9d64
Kelvin-Jesus <kj.avelino.cambiaghi@gmail.com>
2026-07-01 21:35:42 -0300

    mailghost 1.1.1

--- Formula/mailghost.rb
+++ Formula/mailghost.rb
@@ -1,28 +1,28 @@
 class Mailghost < Formula
   desc "Friendly disposable Mail.tm inbox for your terminal"
   homepage "https://github.com/Kelvin-Jesus/mailghost"
-  version "1.1.0"
+  version "1.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Kelvin-Jesus/mailghost/releases/download/v1.1.0/mailghost-v1.1.0-macos-aarch64.tar.gz"
-      sha256 "dfedf6b0bf950589b7d9920d4314e9797e0552179d68ea6f6143f13f5226f978"
+      url "https://github.com/Kelvin-Jesus/mailghost/releases/download/v1.1.1/mailghost-v1.1.1-macos-aarch64.tar.gz"
+      sha256 "fac3b23ac6f52859b94905ccc5a31a46e6acd811e869b9d29f32f4a6c96e40e2"
     end
     on_intel do
-      url "https://github.com/Kelvin-Jesus/mailghost/releases/download/v1.1.0/mailghost-v1.1.0-macos-x86_64.tar.gz"
-      sha256 "9fdc2e7e3e39fb10f0be3267e8001c365269d0c8401426e9696a951188c741bf"
+      url "https://github.com/Kelvin-Jesus/mailghost/releases/download/v1.1.1/mailghost-v1.1.1-macos-x86_64.tar.gz"
+      sha256 "92f27dcb4739947fcf1b215c140d761131873c695eac580bdc3f2819171dd4b9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Kelvin-Jesus/mailghost/releases/download/v1.1.0/mailghost-v1.1.0-linux-aarch64.tar.gz"
-      sha256 "e27e2acb926efa389560508a3210f488703d4ce43afbc7fe0b9c794b903e9dd1"
+      url "https://github.com/Kelvin-Jesus/mailghost/releases/download/v1.1.1/mailghost-v1.1.1-linux-aarch64.tar.gz"
+      sha256 "b837e3056d3a866c387c23ef239cc60f3a59bda401abae5c04d252fec3a027be"
     end
     on_intel do
-      url "https://github.com/Kelvin-Jesus/mailghost/releases/download/v1.1.0/mailghost-v1.1.0-linux-x86_64.tar.gz"
-      sha256 "184d59572ca989ccd2d173a6b908249d70193cbee41f4c2de91eadc6cd028322"
+      url "https://github.com/Kelvin-Jesus/mailghost/releases/download/v1.1.1/mailghost-v1.1.1-linux-x86_64.tar.gz"
+      sha256 "60ed07e7867fbeb6adbee5bf0c9f1974b38ced615c586a6d84d1e09e7ed46196"
     end
   end
 
